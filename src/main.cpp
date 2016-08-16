73c4dd27540c881f9ee5f7c05cdd170f3088b587
seandewar5 <seandewar5@googlemail.com>
2016-08-16 06:07:59 +0100

    Disable console in Windows

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 #include <cstdlib>
 #include <iostream>
 
+#ifdef _WIN32
+#define NOMINMAX
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#endif
+
 #include <SFML/Graphics/RenderWindow.hpp>
 #include <SFML/Window/Event.hpp>
 
@@ -9,7 +15,7 @@
 
 int main(int argc, char* argv[])
 {
-    sf::RenderWindow window(sf::VideoMode(1024, 768), "The File System Dungeon");
+    sf::RenderWindow window(sf::VideoMode(1024, 768), "The File System Dungeon - Loading...");
 
     const auto targetFrameRate = static_cast<unsigned int>(
         std::roundf(1000000.0f / Game::FrameTimeStep.asMicroseconds())
@@ -25,6 +31,8 @@ int main(int argc, char* argv[])
         std::cerr << "ERROR - Failed to init game! Exiting\n";
         return EXIT_FAILURE;
     }
+
+    window.setTitle("The File System Dungeon");
 
 	while (window.isOpen()) {
 		// handle window events
@@ -62,3 +70,11 @@ int main(int argc, char* argv[])
     std::cout << "Exiting game\n";
     return EXIT_SUCCESS;
 }
+
+
+#ifdef _WIN32
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    return main(__argc, __argv);
+}
+#endif
